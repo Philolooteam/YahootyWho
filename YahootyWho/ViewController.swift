df2b592e293b7ef36d73af559c20e99bba525d73
Philip Oloo <info@spacialists.com>
2018-08-15 14:53:36 -0700

    Initial Commit

---
+++ YahootyWho/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YahootyWho
+//
+//  Created by Philip Oloo on 8/15/18.
+//  Copyright © 2018 Philip Oloo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
